df45cbd0928b9f93cfcd79b2742681672a1082c0
macbook <macbook@MacBook-Pro.local>
2026-06-11 15:55:59 +0300

    Fix eslint warnings for clean build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import PrivateRoute from './components/PrivateRoute';
-
 import Landing from './pages/Landing';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -11,54 +10,54 @@ import AdminDashboard from './pages/AdminDashboard';
 import ProductDetail from './pages/ProductDetail';
 import Profile from './pages/Profile';
 import Cart from './pages/Cart';
+import AffiliateDashboard from './pages/AffiliateDashboard';
 
 export default function App() {
     return (
-    <AuthProvider>
-        <BrowserRouter>
-            <div style={{width:'100%', overflowX:'hidden'}}>
-                <Routes>
-                    <Route path="/" element={<Landing />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    import AffiliateDashboard from './pages/AffiliateDashboard';
-// inside <Routes>:
-<Route path="/affiliate" element={<AffiliateDashboard />} />
-                    <Route path="/product/:id" element={
-    <PrivateRoute roles={['buyer', 'seller', 'admin']}>
-        <ProductDetail />
-    </PrivateRoute>
-} />
-
-<Route path="/profile" element={
-    <PrivateRoute roles={['buyer', 'seller', 'admin']}>
-        <Profile />
-    </PrivateRoute>
-} />
-
-<Route path="/cart" element={
-    <PrivateRoute roles={['buyer']}>
-        <Cart />
-    </PrivateRoute>
-} />
-                    <Route path="/buyer" element={
-                        <PrivateRoute roles={['buyer']}>
-                            <BuyerDashboard />
-                        </PrivateRoute>
-                    } />
-
-                    <Route path="/seller" element={
-                        <PrivateRoute roles={['seller']}>
-                            <SellerDashboard />
-                        </PrivateRoute>
-                    } />
-
-                    <Route path="/admin" element={
-                        <PrivateRoute roles={['admin']}>
-                            <AdminDashboard />
-                        </PrivateRoute>
-                    } />
-                </Routes> </div>
+        <AuthProvider>
+            <BrowserRouter>
+                <div style={{ width: '100%', overflowX: 'hidden' }}>
+                    <Routes>
+                        <Route path="/" element={<Landing />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/affiliate" element={
+                            <PrivateRoute roles={['affiliate']}>
+                                <AffiliateDashboard />
+                            </PrivateRoute>
+                        } />
+                        <Route path="/product/:id" element={
+                            <PrivateRoute roles={['buyer', 'seller', 'admin']}>
+                                <ProductDetail />
+                            </PrivateRoute>
+                        } />
+                        <Route path="/profile" element={
+                            <PrivateRoute roles={['buyer', 'seller', 'admin']}>
+                                <Profile />
+                            </PrivateRoute>
+                        } />
+                        <Route path="/cart" element={
+                            <PrivateRoute roles={['buyer']}>
+                                <Cart />
+                            </PrivateRoute>
+                        } />
+                        <Route path="/buyer" element={
+                            <PrivateRoute roles={['buyer']}>
+                                <BuyerDashboard />
+                            </PrivateRoute>
+                        } />
+                        <Route path="/seller" element={
+                            <PrivateRoute roles={['seller']}>
+                                <SellerDashboard />
+                            </PrivateRoute>
+                        } />
+                        <Route path="/admin" element={
+                            <PrivateRoute roles={['admin']}>
+                                <AdminDashboard />
+                            </PrivateRoute>
+                        } />
+                    </Routes>
+                </div>
             </BrowserRouter>
         </AuthProvider>
     );
